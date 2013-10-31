28e778897b10c3fced66a97fe6991036f69fac12
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-10-31 15:37:32 +0100

    Install groovy, gradle, griffon.

---
+++ groovy/env.zsh
@@ -0,0 +1,2 @@
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
+export GRIFFON_HOME=/usr/local/Cellar/griffon/1.4.0/libexec
---
+++ groovy/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+brew install groovy --invokedynamic
+brew install gradle griffon
+
+exit 0
