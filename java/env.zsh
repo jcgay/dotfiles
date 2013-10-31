a109e50ae739d71f1bd32790cbd781edac0316d3
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-10-31 14:25:41 +0100

    Install jEnv.

--- java/env.zsh
+++ java/env.zsh
@@ -1,1 +1,5 @@
 export JAVA_HOME=$(/usr/libexec/java_home)
+
+# jEnv configuration.
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
--- java/install.sh
+++ java/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "\033[0;34mCloning jEnv...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone https://github.com/gcuisinier/jenv.git ~/.jenv || {
+  echo "git not installed"
+  exit
+}
+
 wget -P $TMPDIR --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-macosx-x64.dmg"
 open $TMPDIR/jdk-7u45-macosx-x64.dmg
 
