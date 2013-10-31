69f68081058b7d48b223ac10f3a4d99d772d522c
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-10-31 17:46:31 +0100

    Add zsh-syntax-highlighting.

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,4 +5,11 @@ brew install zsh
 #oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
+#zsh-syntax-highlighting
+echo "\033[0;34mCloning zsh-syntax-highlighting...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || {
+  echo "git not installed"
+  exit
+}
+
 exit 0
