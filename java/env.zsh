cdd34418ee23f940fa6aa68b9c2e541f5384a0b9
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-11-01 16:57:18 +0100

    Activate jEnv plugins.

--- java/env.zsh
+++ java/env.zsh
@@ -3,3 +3,9 @@
 # jEnv configuration.
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
+
+jenv enable-plugin maven > /dev/null 2>&1
+jenv enable-plugin gradle > /dev/null 2>&1
+jenv enable-plugin groovy > /dev/null 2>&1
+jenv enable-plugin sbt > /dev/null 2>&1
+jenv enable-plugin scala > /dev/null 2>&1
