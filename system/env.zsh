657718a4b39ddfd8f516a73bf807ba8511f067a6
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-11-05 22:43:59 +0100

    Move jEnv init to keep $PATH order.
    It makes plugins work !

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,13 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export EDITOR="mate -w"
+
+# jEnv configuration.
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+jenv enable-plugin maven > /dev/null 2>&1
+jenv enable-plugin gradle > /dev/null 2>&1
+jenv enable-plugin groovy > /dev/null 2>&1
+jenv enable-plugin sbt > /dev/null 2>&1
+jenv enable-plugin scala > /dev/null 2>&1
+
