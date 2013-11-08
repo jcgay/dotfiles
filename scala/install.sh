7f811cd128e92d6dc1dc8e45b18631e1364fbfb9
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-11-08 20:43:38 +0100

    Update sbt-idea to 1.5.2.

--- scala/install.sh
+++ scala/install.sh
@@ -4,6 +4,6 @@ brew install scala --with-docs
 brew install sbt
 
 mkdir -p ~/.sbt/0.13/plugins/
-echo 'addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")' >> ~/.sbt/0.13/plugins/build.sbt
+echo 'addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")' >> ~/.sbt/0.13/plugins/build.sbt
 
 exit 0
