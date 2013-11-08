918ba688ca4fd0b8368f98a8a5f5facc1e654f84
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-11-08 20:40:24 +0100

    Install android-file-transfer.

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager
+brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer
 
 exit 0
 
