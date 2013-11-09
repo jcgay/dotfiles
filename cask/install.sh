23512a2210b3ede419c96b03fed48858df8cd35a
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-11-09 22:57:59 +0100

    Install nvalt.

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer
+brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt
 
 exit 0
 
