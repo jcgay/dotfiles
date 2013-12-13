acd7c97ce7252887c393cb463fe94be5c8685de0
Hadrien Collongues <chadrien@chadrien.fr>
2013-12-13 09:50:35 +0100

    Better `cd` in installer
    With `cd "$(dirname $)"/..` it works bad, because `$(dirname $)` will return `.` so it implies that to correctly works, you have to be in the `script` directory, otherwise if you're e.g. in `$HOME/.dotfiles` you'll go in `$HOME` and then look for all `install.sh`, which may not be what you intended to do.
    
    Using `$(dirname $0)` instead ensure that you'll go one level higher than the `install` script.
    Conflicts:
    	script/install

--- script/install.sh
+++ script/install.sh
@@ -16,7 +16,7 @@ then
 	brew install brew-cask
 fi
 
-cd "$(dirname $)"/..
+cd "$(dirname $0)"/..
 
 # find the installers and run them iteratively
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
