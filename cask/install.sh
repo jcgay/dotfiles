19bd74685b2c918ad353ebbe2f34278b60125bf8
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-12-25 20:42:15 +0100

    Install Adium.

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver
+brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver adium
 
 exit 0
 
