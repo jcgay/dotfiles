5fca6a7c7147927183ceef87a6956fee658be93d
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-01-03 21:17:58 +0100

    Install menumeters.

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver adium
+brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver adium menumeters
 
 exit 0
 
