a2372594a31b16e8e3383acad410d62d3d9cbcf0
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-02-01 11:25:51 +0100

    Add quick look plugins.

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver adium menumeters
+brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver adium menumeters qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
 
 exit 0
 
