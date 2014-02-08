7fd0e0ea36813517ec87a4e82bc2d9b931511785
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-02-08 11:35:33 +0100

    Add some configuration (man, hist, grep).

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,15 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export EDITOR="mate -w"
 
+# Make some commands not show up in history
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X"
+
+# Always enable colored `grep` output
+export GREP_OPTIONS="--color=auto"
+
 # jEnv configuration.
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
@@ -10,4 +19,3 @@ jenv enable-plugin gradle > /dev/null 2>&1
 jenv enable-plugin groovy > /dev/null 2>&1
 jenv enable-plugin sbt > /dev/null 2>&1
 jenv enable-plugin scala > /dev/null 2>&1
-
