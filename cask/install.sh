56e085a1c4a84231cd41c1f4632bc0b68411297f
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-02-08 11:26:54 +0100

    Make a function to install brew casks.

--- cask/install.sh
+++ cask/install.sh
@@ -1,6 +1,52 @@
 #!/bin/sh
 
-brew cask install alfred calibre chromium controlplane dash dropbox cyberduck firefox gimp google-chrome google-hangouts intellij-idea-ultimate iterm2 lastpass-universal mou p4merge sourcetree steam textmate the-unarchiver virtualbox vlc libre-office google-music-manager android-file-transfer nvalt spotify tagr disk-inventory-x pandoc openemu xbox360-controller-driver adium menumeters qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
+function installcask() {
+	brew cask install "${@}" 2> /dev/null
+}
+
+installcask alfred
+installcask calibre
+installcask chromium
+installcask controlplane
+installcask dash
+installcask dropbox
+installcask cyberduck
+installcask firefox
+installcask gimp
+installcask google-chrome
+installcask google-hangouts
+installcask intellij-idea-ultimate
+installcask iterm2
+installcask lastpass-universal
+installcask mou
+installcask p4merge
+installcask sourcetree
+installcask steam
+installcask textmate
+installcask the-unarchiver
+installcask virtualbox
+installcask vlc
+installcask libre-office
+installcask google-music-manager
+installcask android-file-transfer
+installcask nvalt
+installcask spotify
+installcask tagr
+installcask disk-inventory-x
+installcask pandoc
+installcask openemu
+installcask xbox360-controller-driver
+installcask adium
+installcask menumeters
+installcask qlcolorcode
+installcask qlstephen
+installcask qlmarkdown
+installcask quicklook-json
+installcask qlprettypatch
+installcask quicklook-csv
+installcask betterzipql
+installcask webp-quicklook
+installcask suspicious-package
 
 exit 0
 
