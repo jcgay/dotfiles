423707397098f9c369b63c179bde84f5b87a1303
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-02-22 20:15:56 +0100

    Add python environment.

---
+++ python/install.sh
@@ -0,0 +1,14 @@
+brew install python
+pip install virtualenv
+
+## Install Pelican
+mkdir -p ~/virtualenvs/pelican
+virtualenv ~/virtualenvs/pelican
+cd ~/virtualenvs/pelican
+. bin/activate
+pip install pelican
+pip install Markdown
+pip install beautifulsoup4
+pip install typogrify
+
+exit 0
