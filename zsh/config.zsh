0d0247bf02b64d16ef850170115d444824115471
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-03-02 20:20:04 +0100

    Manage zsh plugins with antigen.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,27 +2,31 @@ fpath=($DOTFILES/functions $fpath)
 
 autoload -U $DOTFILES/functions/*(:t)
 
-# Path to your oh-my-zsh configuration.
-ZSH=$HOME/.oh-my-zsh
-
-# Set name of the theme to load.
-# Look in ~/.oh-my-zsh/themes/
-# Optionally, if you set this to "random", it'll load a random theme each
-# time that oh-my-zsh is loaded.
-ZSH_THEME="robbyrussell"
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git brew mvn textmate extract gitignore gradle history sbt scala common-aliases z zsh-syntax-highlighting)
-
-source $ZSH/oh-my-zsh.sh
-
 # Antigen
-source "$HOME/.antigen/antigen.zsh"
+source "$HOME/.dotfiles/antigen/antigen.zsh"
 
-antigen-use oh-my-zsh
-antigen-bundle arialdomartini/oh-my-git
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+# Plugins
+antigen-bundles <<EOBUNDLES
+
+git
+brew
+mvn
+textmate
+extract
+gitignore
+gradle
+history
+sbt
+scala
+z
+arialdomartini/oh-my-git
+zsh-users/zsh-history-substring-search
+zsh-users/zsh-syntax-highlighting
+
+EOBUNDLES
+
 antigen theme arialdomartini/oh-my-git-themes arialdo-pathinline
 
 antigen-apply
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,20 +2,4 @@
 
 brew install zsh
 
-#oh-my-zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-#zsh-syntax-highlighting
-echo "\033[0;34mCloning zsh-syntax-highlighting...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting || {
-  echo "git not installed"
-  exit
-}
-
-echo "\033[0;34mCloning antigen...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/zsh-users/antigen.git ~/.antigen || {
-  echo "git not installed"
-  exit
-}
-
 exit 0
