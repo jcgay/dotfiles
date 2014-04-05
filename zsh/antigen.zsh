6c1ddb08445c0dac6c58324de5b65ae5ce1a5e41
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-04-05 11:55:56 +0200

    Extract antigen configuration.

---
+++ zsh/antigen.zsh
@@ -0,0 +1,29 @@
+# Antigen
+source "$HOME/.dotfiles/antigen/antigen.zsh"
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+# Plugins
+antigen-bundles <<EOBUNDLES
+
+git
+brew
+mvn
+textmate
+extract
+gitignore
+gradle
+history
+sbt
+scala
+z
+arialdomartini/oh-my-git
+supercrabtree/k
+zsh-users/zsh-history-substring-search
+zsh-users/zsh-syntax-highlighting
+
+EOBUNDLES
+
+antigen theme arialdomartini/oh-my-git-themes arialdo-pathinline
+
+antigen-apply
