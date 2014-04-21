2378c81b61467b3aa64ae59753f29f9121d02d4b
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-04-21 16:09:01 +0200

    Update Java 7u55 and Java 8u5

--- java/install.sh
+++ java/install.sh
@@ -9,10 +9,10 @@ hash git >/dev/null && /usr/bin/env git clone https://github.com/gcuisinier/jenv
 wget -P $TMPDIR http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg
 open $TMPDIR/JavaForOSX2013-05.dmg
 
-wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-macosx-x64.dmg"
-open $TMPDIR/jdk-7u51-macosx-x64.dmg
+wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u55-b13/jdk-7u55-macosx-x64.dmg"
+open $TMPDIR/jdk-7u55-macosx-x64.dmg
 
-wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-macosx-x64.dmg"
-open $TMPDIR/jdk-8-macosx-x64.dmg
+wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-macosx-x64.dmg"
+open $TMPDIR/jdk-8u5-macosx-x64.dmg
 
 exit 0
