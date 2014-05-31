3500427e67f41bdb86cc3f9e338d89e98b96a263
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-05-31 09:22:49 +0200

    Install Java 7u60

--- java/install.sh
+++ java/install.sh
@@ -9,8 +9,8 @@ hash git >/dev/null && /usr/bin/env git clone https://github.com/gcuisinier/jenv
 wget -P $TMPDIR http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg
 open $TMPDIR/JavaForOSX2013-05.dmg
 
-wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u55-b13/jdk-7u55-macosx-x64.dmg"
-open $TMPDIR/jdk-7u55-macosx-x64.dmg
+wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u60-b19/jdk-7u60-macosx-x64.dmg"
+open $TMPDIR/jdk-7u60-macosx-x64.dmg
 
 wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-macosx-x64.dmg"
 open $TMPDIR/jdk-8u5-macosx-x64.dmg
