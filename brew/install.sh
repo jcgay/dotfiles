c906fbaa7da24f1dd7188007178a55466aa0c6a9
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-02-14 10:38:19 +0100

    Install the_silver_searcher (ag)

--- brew/install.sh
+++ brew/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-brew install wget htop-osx markdown terminal-notifier grc libjpg optipng gnu-sed gpg node cheat lastpass-cli
+brew install wget htop-osx markdown terminal-notifier grc libjpg optipng gnu-sed gpg node cheat lastpass-cli the_silver_searcher
 
 exit 0
