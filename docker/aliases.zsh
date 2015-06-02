d2d1035b8a70f8b38bd28b53dfc949134de09a70
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-06-02 21:25:01 +0200

    Add docker aliases

---
+++ docker/aliases.zsh
@@ -0,0 +1,2 @@
+alias dockerclean="docker ps -a | grep 'days ago\|weeks ago' | awk '{print $1}' | gxargs --no-run-if-empty docker rm"
+alias dockercleani="docker images | grep '<none>' | awk '{print $3}' | gxargs --no-run-if-empty docker rmi -f"
