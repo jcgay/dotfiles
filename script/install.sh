a30bcddc24ccdc5c5cf1cd08330d38bc8fa2feed
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-07-05 17:23:30 +0200

    Do not infinite loop when running install.sh

--- script/install.sh
+++ script/install.sh
@@ -20,7 +20,7 @@ fi
 cd "$(dirname $0)"/..
 
 # find the installers and run them iteratively
-find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
+find . -name install.sh -not -path "*/script/*" | while read installer ; do sh -c "${installer}" ; done
 
 # Configure hostname (http://biomedicalontologies.com/2012/11/14/fixing-java-net-local-host-name-unknown-error-on-mac-os-x/)
 scutil –set HostName “localhost”
