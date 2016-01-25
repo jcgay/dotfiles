1171154f86f9aa2d3ca1688c1be7cb8d0cc37c9b
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-01-25 22:42:19 +0100

    Replace boot2docker by docker-machine

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-brew install docker boot2docker
+brew install docker docker-machine docker-compose xhyve docker-machine-driver-xhyve
+
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+
+docker-machine create --driver virtualbox dev
+docker-machine create xhyve --driver xhyve --xhyve-experimental-nfs-share
 
 exit 0
