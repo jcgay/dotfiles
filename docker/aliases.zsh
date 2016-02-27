ea0721e69c9e3ceb1f2f40beed61cc3c2bf7d00e
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-02-27 11:31:04 +0100

    Add aliases for docker-machine and docker-compose

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,4 @@
 alias dockerclean="docker ps -a | grep 'days ago\|weeks ago' | awk '{print $1}' | gxargs --no-run-if-empty docker rm"
 alias dockercleani="docker images | grep '<none>' | awk '{print $3}' | gxargs --no-run-if-empty docker rmi -f"
+alias dm="docker-machine"
+alias dco="docker-compose"
