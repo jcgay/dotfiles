d837a3b944d7cf17fcc2be2a615a8875c98d7cec
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-04-29 20:06:07 +0200

    Install kitematic

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 brew install docker docker-machine docker-compose xhyve docker-machine-driver-xhyve
+cask install kitematic
 
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
