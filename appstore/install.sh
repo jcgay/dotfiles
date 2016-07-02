ea251d072b2441f19a850a00f1976dd6507e1dbc
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-07-02 21:47:43 +0200

    Install Mas to manage AppStore with a CLI
    https://github.com/argon/mas

---
+++ appstore/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+brew install mas
+
+mas install 497799835 # Xcode
+mas install 409789998 # Twitter
+mas install 467939042 # Growl
+mas install 948415170 # Pushbullet
+mas install 715768417 # Microsoft Remote Desktop
+mas install 890031187 # Marked 2
+mas install 557168941 # Tweetbot
+mas install 926036361 # LastPass
+
+exit 0
