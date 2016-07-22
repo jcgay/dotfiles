86744fe0618c76ef1d69833bd5c45555962ed57b
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-07-22 22:04:37 +0200

    Upgrade to JDK 8u101

--- java/install.sh
+++ java/install.sh
@@ -12,7 +12,7 @@ open $TMPDIR/javaforosx.dmg
 wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-macosx-x64.dmg"
 open $TMPDIR/jdk-7u79-macosx-x64.dmg
 
-wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-macosx-x64.dmg"
-open $TMPDIR/jdk-8u91-macosx-x64.dmg
+wget -P $TMPDIR --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-macosx-x64.dmg"
+open $TMPDIR/jdk-8u101-macosx-x64.dmg
 
 exit 0
