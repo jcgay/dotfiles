8a89f7782eeb784064b3f2b0cc365e9ebd99b1a7
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-09-03 15:56:29 +0200

    Use docker for mac

--- docker/install.sh
+++ docker/install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-brew install docker docker-machine docker-compose xhyve docker-machine-driver-xhyve
+cask install docker
 cask install kitematic
-
-sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-
-docker-machine create --driver virtualbox dev
-docker-machine create xhyve --driver xhyve --xhyve-experimental-nfs-share
 
 exit 0
