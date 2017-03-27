f6fbd357f67afa7dbf2482c32ab03b21c8365219
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-03-27 21:49:49 +0200

    Install libav (for ffprobe)

--- brew/install.sh
+++ brew/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-brew install wget htop-osx markdown terminal-notifier grc libjpg optipng gnu-sed gpg node cheat lastpass-cli the_silver_searcher grep pandoc youtube-dl shellcheck
+brew install wget htop-osx markdown terminal-notifier grc libjpg optipng gnu-sed gpg node cheat lastpass-cli the_silver_searcher grep pandoc youtube-dl libav shellcheck
 
 exit 0
