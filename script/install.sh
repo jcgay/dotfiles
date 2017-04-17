07a04790d0cb1ce8c23dbf117eaf5b247090a1b6
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-04-17 19:32:20 +0200

    Update brew install URL

--- script/install.sh
+++ script/install.sh
@@ -10,7 +10,7 @@ then
 	if test ! $(which brew)
 	then
 		echo "  Installing Homebrew for you."
-		ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 	fi
     brew tap Homebrew/bundle
 	brew tap caskroom/cask
