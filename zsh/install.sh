fa2dbf2a6c10bde50e66ebe1163a50a346c43cdd
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-04-17 21:40:12 +0200

    Set zsh as default shell

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,6 +4,8 @@ echo 'Installing zsh...'
 
 brew install zsh
 
+chsh -s /bin/zsh
+
 # Install fonts for oh-my-git theme
 wget -P $TMPDIR https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro%2BPowerline%2BAwesome%2BRegular.ttf
 mv $TMPDIR/SourceCodePro+Powerline+Awesome+Regular.ttf /Library/Fonts/
