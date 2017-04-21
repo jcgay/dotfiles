d0cc9b8d0c514bbca8e5c4ee6027777120d45d67
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2017-04-21 12:24:27 +0200

    Add Install4J

---
+++ install4j/env.zsh
@@ -0,0 +1,1 @@
+export INSTALL4J_HOME_5_1_15="/Applications/install4j-5.1.15"
---
+++ install4j/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget -P $TMPDIR http://download-keycdn.ej-technologies.com/install4j/install4j_macos_java7_5_1_15_with_jre.dmg
+open $TMPDIR/install4j_macos_java7_5_1_15_with_jre.dmg
+
+exit 0
