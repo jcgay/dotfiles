5af7be7cb6c74c9feb4ec72a6b672569ca83f04d
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2017-04-21 12:45:54 +0200

    Remove stuff I don't use @ work

--- python/install.sh
+++ python/install.sh
@@ -6,16 +6,4 @@ brew install python
 brew install python3
 pip install virtualenv
 
-## Install Pelican
-mkdir -p ~/virtualenvs/pelican
-virtualenv ~/virtualenvs/pelican
-cd ~/virtualenvs/pelican
-. bin/activate
-pip install pelican
-pip install Markdown
-pip install beautifulsoup4
-pip install typogrify
-pip install webassets
-pip install cssmin
-
 exit 0
