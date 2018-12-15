5c84b04a9f406ceb6aa1389f0837ed780427d78e
Jean-Christophe Gay <contact@jeanchristophegay.com>
2018-12-15 14:58:55 +0100

    Replace oh-my-git theme by powerlevel9k
    https://github.com/bhilburn/powerlevel9k

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,8 +7,7 @@ brew install getantibody/tap/antibody
 
 chsh -s /bin/zsh
 
-# Install fonts for oh-my-git theme
-wget -P $TMPDIR https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro%2BPowerline%2BAwesome%2BRegular.ttf
-mv $TMPDIR/SourceCodePro+Powerline+Awesome+Regular.ttf /Library/Fonts/
+# Install fonts for powerlevel9k theme
+brew cask install font-hack-nerd-font
 
 exit 0
