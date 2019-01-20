01df2949548284ef284d15b57777770756cf500e
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-01-20 18:28:42 +0100

    Remove oh-my-zsh git plugin and add missing ones
    the git plugin always give me errors... and I don't use all the aliases.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,2 +1,8 @@
 alias git='LANG=en_US.UTF-8 git'
+alias g='git'
+alias gc='git commit -v'
+alias gco='git checkout'
+alias gcp='git cherry-pick'
+alias grt='cd $(git rev-parse --show-toplevel || echo ".")'
+
 alias pgr=parallel-git-repo
