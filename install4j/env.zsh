f962ec29874e69320877cd92b8f462fa7fa47050
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2019-02-12 11:51:10 +0100

    Install Install4J 7.0.9

--- install4j/env.zsh
+++ install4j/env.zsh
@@ -1,1 +1,2 @@
 export INSTALL4J_HOME_5_1_15="/Applications/install4j-5.1.15"
+export INSTALL4J_HOME_7_0_9="/Applications/install4j.app/Contents/Resources/app"
--- install4j/install.sh
+++ install4j/install.sh
@@ -3,4 +3,7 @@
 wget -P $TMPDIR http://download-keycdn.ej-technologies.com/install4j/install4j_macos_java7_5_1_15_with_jre.dmg
 open $TMPDIR/install4j_macos_java7_5_1_15_with_jre.dmg
 
+wget -P $TMPDIR https://download-keycdn.ej-technologies.com/install4j/install4j_macos_7_0_9.dmg
+open $TMPDIR/install4j_macos_7_0_9.dmg
+
 exit 0
