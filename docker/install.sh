c455f60581912b0560ef9b663701e052b984dcc3
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-05-05 00:10:10 +0200

    Correctly use dive tap for installation

--- docker/install.sh
+++ docker/install.sh
@@ -5,7 +5,7 @@ echo 'Installing Docker...'
 brew cask install docker
 brew cask install kitematic
 brew install ctop
-brew install dive
+brew install wagoodman/dive/dive
 brew install moul/moul/docker-diff
 
 exit 0
