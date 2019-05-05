fc5cb066375184cca4215b112bb4293ea6c5e0e4
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-05-05 09:32:16 +0200

    Add gst alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,5 +4,6 @@ alias gc='git commit -v'
 alias gco='git checkout'
 alias gcp='git cherry-pick'
 alias grt='cd $(git rev-parse --show-toplevel || echo ".")'
+alias gst='git status'
 
 alias pgr=parallel-git-repo
