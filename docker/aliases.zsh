4841821499a6ae65dbde856310723354251e1912
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2019-12-26 06:56:05 +0100

    Add alias to stop all docker containers

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,4 +1,5 @@
 alias dockerclean="docker ps -a | grep 'days ago\|weeks ago' | awk '{print $1}' | gxargs --no-run-if-empty docker rm"
 alias dockercleani="docker images | grep '<none>' | awk '{print $3}' | gxargs --no-run-if-empty docker rmi -f"
+alias dockerstop="docker stop $(docker ps -a -q)"
 alias dm="docker-machine"
 alias dco="docker-compose"
