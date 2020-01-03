e5ce0a03a58eca92c7e932f3520b4fd71a22a8d4
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2020-01-03 10:27:23 +0100

    Hide Java icon when launching Gradle
    headless was not enough...
    
    https://stackoverflow.com/questions/17460777/stop-java-coffee-cup-icon-from-appearing-in-the-dock-on-mac-osx

--- jvm/env.zsh
+++ jvm/env.zsh
@@ -1,7 +1,7 @@
 export JAVA_HOME=$(/usr/libexec/java_home)
 
 export GROOVY_HOME=~/.sdkman/candidates/groovy/current
-export GRADLE_OPTS=-Djava.awt.headless=true
+export GRADLE_OPTS="-Djava.awt.headless=true -Dapple.awt.UIElement=true"
 
 export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 
