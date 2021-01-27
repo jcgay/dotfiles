b4f218b91b272354e31a54818e82021ef8dbb52a
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2021-01-27 16:36:49 +0100

    Add Install4J 8.x

--- install4j/env.zsh
+++ install4j/env.zsh
@@ -1,2 +1,3 @@
 export INSTALL4J_HOME_5_1_15="/Applications/install4j-5.1.15"
 export INSTALL4J_HOME_7_0_9="/Applications/install4j.app/Contents/Resources/app"
+export INSTALL4J_HOME_8_0_10="/Applications/install4j 8.app/Contents/Resources/app"
