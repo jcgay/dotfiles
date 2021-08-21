506a18c4ec6a1e961f4197df7108ecd03fb69d29
Jean-Christophe Gay <contact@jeanchristophegay.com>
2021-08-21 15:58:02 +0200

    Install mvnd
    https://github.com/mvndaemon/mvnd

--- maven/install.sh
+++ maven/install.sh
@@ -2,9 +2,10 @@
 
 echo 'Installing Maven...'
 
-brew install maven
+brew install maven mvndaemon/homebrew-mvnd/mvnd
 
 # install maven-color, maven-notifier and maven-profiler
 brew unlink maven && brew install maven-deluxe
+brew unlink mvnd && brew install mvnd-deluxe
 
 exit 0
