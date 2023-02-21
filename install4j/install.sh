8e1e48a507afcd27e3db89159a5097a81a7b0cca
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2023-02-21 05:34:44 +0100

    Install Install4J 8.0.11

--- install4j/install.sh
+++ install4j/install.sh
@@ -6,4 +6,7 @@ open $TMPDIR/install4j_macos_java7_5_1_15_with_jre.dmg
 wget -P $TMPDIR https://download-keycdn.ej-technologies.com/install4j/install4j_macos_7_0_9.dmg
 open $TMPDIR/install4j_macos_7_0_9.dmg
 
+wget -P $TMPDIR https://download-gcdn.ej-technologies.com/install4j/install4j_macos_8_0_11.dmg
+open $TMPDIR/install4j_macos_8_0_11.dmg
+
 exit 0
