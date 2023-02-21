be5b3ce2da5e2faea4a7d689f269d1c33288a7c9
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2023-02-21 05:37:15 +0100

    Replace docker-sw2 aliases by script

---
+++ docker/env.zsh
@@ -0,0 +1,3 @@
+export DOCKERSW2_CERT="/Users/jcgay/.dockersw2/client-jenkins-cert.pem"
+export DOCKERSW2_CACERT="/Users/jcgay/.dockersw2/ca.pem"
+export DOCKERSW2_KEY="/Users/jcgay/.dockersw2/client-jenkins-key.pem"
