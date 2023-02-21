442ba0f4b8a44e5c56ed3f67933575d9ff24ed04
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2023-02-21 06:11:08 +0100

    Growl does not exist anymore 😢

--- appstore/install.sh
+++ appstore/install.sh
@@ -5,7 +5,6 @@ echo 'Installing from the App Store...'
 brew install mas
 
 mas install 497799835 # Xcode
-mas install 467939042 # Growl
 mas install 948415170 # Pushbullet
 mas install 1295203466 # Microsoft Remote Desktop
 mas install 890031187 # Marked 2
