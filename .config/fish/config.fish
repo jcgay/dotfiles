2e9cfecdc83c05a2270ebceab27b547288a7aca4
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2026-08-21 12:48:05 +0200

    Add docker in PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+# The following lines were added by Docker Desktop to add commands to your PATH.
+export PATH="$PATH:/Users/jcgay/.docker/bin"
+# End of Docker Desktop section.
+
 # Starship prompt — must be in config.fish (conf.d/ loads before this file)
 starship init fish | source
 
